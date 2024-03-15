028ae07d9862a8b8c4e20a012c151914f546f4eb
Andrew Virts <asvirts@outlook.com>
2024-03-14 20:21:16 -0500

    Plant type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,47 @@
 "use client"
 
-import { useState } from "react"
-import axios from "axios"
+import {
+  AwaitedReactNode,
+  JSXElementConstructor,
+  ReactElement,
+  ReactNode,
+  ReactPortal,
+  useState
+} from "react"
 
 export default function Home() {
   const [response, setResponse] = useState(null)
 
-  const Request = async () => {
-    try {
-      const result = await axios.get(
-        `https://perenual.com/api/species-list?key=sk-uP4E65f243b96643e4567`,
-        { someData: "test" }
-      ) // Replace the data with your actual payload
-      setResponse(result.data)
-    } catch (error) {
-      console.error("Error occured:", error)
-      setResponse(error.message)
+  // const Request = async () => {
+  //   try {
+  //     const result = await axios.get(
+  //       `https://perenual.com/api/species-list?key=sk-uP4E65f243b96643e4567`,
+  //       { someData: "test" }
+  //     ) // Replace the data with your actual payload
+  //     setResponse(result.data)
+  //   } catch (error) {
+  //     console.error("Error occured:", error)
+  //     setResponse(error.message)
+  //   }
+  // }
+
+  async function getData() {
+    const res = await fetch(
+      "https://perenual.com/api/species-list?key=sk-uP4E65f243b96643e4567"
+    )
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data")
     }
+
+    const data = await res.json()
+
+    setResponse(data)
   }
 
   return (
     <div>
-      <button onClick={Request}>Download</button>
+      <button onClick={getData}>Download</button>
       {response && <div>Response: {JSON.stringify(response)}</div>}
     </div>
   )
---
+++ types.ts
@@ -0,0 +1,16 @@
+type Plant = {
+  id: number
+  common_name: string
+  scientific_name: string[]
+  other_name: string[]
+  cycle: string
+  watering: string
+  sunlight: string[]
+  default_image: object
+  license_url: string
+  original_url: string
+  regular_url: string
+  medium_url: string
+  small_url: string
+  thumbnail: string
+}
